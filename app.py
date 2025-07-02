355867ce6c61429867c750655396a533bf321c4e
A1-lex <alemaina867@gmail.com>
2025-07-02 12:48:36 +0300

    .gitignore and untrack unwanted files

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def home():
 
     return render_template('index.html', team_members=team_members)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
