281ee6459dfed3eb40fde6b9197904ec0d071fa5
A1-lex <alemaina867@gmail.com>
2025-07-02 12:32:36 +0300

    Add .gitignore and .dockerignore

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 def home():
     team_members = [
     {'img':'boni.png',    'name':'Dr. Nderu',       'role':'Supervising Director',            'link':'https://linkedin.com/in/drnderu'},
-    {'img':'boni.png',    'name':'Mr.Robert',       'role':'Mentor & AI Specialist',          'link':'https://linkedin.com/in/faithnjeri'},
-    {'img':'Mnisaa.jpg',  'name':'John Gitau',      'role':'UI/UX Designer',                  'link':'https://linkedin.com/in/brianotieno'},
+    {'img':'robert.png',  'name':'Mr.Robert',       'role':'Mentor & AI Specialist',          'link':'https://www.linkedin.com/in/robert-ndungu/'},
+    {'img':'mnisaa.jpg',  'name':'John Gitau',      'role':'UI/UX Designer',                  'link':'https://linkedin.com/in/brianotieno'},
     {'img':'boni.jpg',    'name':'Boniface Manda',  'role':'Security Analyst',                'link':'https://linkedin.com/in/boniface'},
-    {'img':'emanuel.png', 'name':'Ruth Khalayi',    'role':'Data Scientist',                  'link':'https://linkedin.com/in/emanuel'},
+    {'img':'layii.png',   'name':'Ruth Khalayi',    'role':'Data Scientist',                  'link':'https://linkedin.com/in/emanuel'},
     {'img':'manu.jpg',    'name':'Emanuel Mudasia', 'role':'Network Security Engineer',       'link':'https://linkedin.com/in/robert'},
     {'img':'joshua.jpg',  'name':'Joshua Mativo',   'role':'Model Trainer',                   'link':'https://linkedin.com/in/john'},
-    {'img':'a1-lex.jpg',  'name':'Alex Maina',      'role':'Full-stack Developer',            'link':'https://linkedin.com/in/kelvin'},
+    {'img':'a1-lex.jpg',  'name':'Alex Maina',      'role':'Full-stack Developer',            'link':'https://www.linkedin.com/in/alex-maina'},
     {'img':'mato.jpg',    'name':'Martin',          'role':'Research & Documentation Lead',   'link':'https://linkedin.com/in/drnderu'},
 ]
 
